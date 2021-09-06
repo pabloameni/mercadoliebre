905e9b87ccae3f53c6a019dbf610490b56880c88
Pablo Menichini <pablo.menichini@gmail.com>
2021-09-06 19:53:01 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+
+app.listen(4000, ()=>{
+    console.log('Servidor corriendo en http://localhost:4000/');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
