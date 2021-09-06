ba7bf82e368d5c6ccda443790bce002518d782e0
Pablo Menichini <pablo.menichini@gmail.com>
2021-09-06 20:25:11 -0300

    Heroku requirements

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-app.listen(4000, ()=>{
-    console.log('Servidor corriendo en http://localhost:4000/');
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=>{
+    console.log('Servidor corriendo en http://localhost:${PORT}/');
 });
 
 app.get('/', (req,res)=>{
